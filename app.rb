c8a27ac5aba8c1a753922817ed6629f0cf106dff
MaevaGilles <maevagilles-web@hotmail.fr>
2022-10-18 22:38:54 +0200

    Commit of the night

--- app.rb
+++ app.rb
@@ -6,42 +6,49 @@ require_relative 'lib/player'
 
 player1 = Player.new("Josianne")
 player2 = Player.new("José")
+human1 = HumanPlayer.new("Maëva")
 
-puts "[DEBUT DU JEU]"
-sleep 1
+ human1.search_health_pack
 
-puts "Ladies & Gentlemen BIENVENUUUUUE"
-sleep 1
+#puts "[DEBUT DU JEU]"
+#sleep 1
 
-puts "À ma droite #{player1.name}!"
-sleep 1
+#puts "Ladies & Gentlemen BIENVENUUUUUE"
+#sleep 1
 
-puts "À ma gauche #{player2.name}!"
-sleep 1
+#puts "À ma droite #{player1.name}!"
+#sleep 1
 
-x = 1
-while player1.life_points>0 && player2.life_points>0 
+#puts "À ma gauche #{player2.name}!"
+#sleep 1
 
-    puts "Voici l'état de chaque joueur : "
-    sleep 1
+#x = 1
+#while player1.life_points>0 && player2.life_points>0 
 
-    puts player1.show_state
-    sleep 1
+   # puts "Voici l'état de chaque joueur : "
+   # sleep 1
 
-    puts player2.show_state
-    sleep 1
+  #  puts player1.show_state
+   # sleep 1
 
-    puts "-------ROUND #{x}-------"
-    x += 1
+  #  puts player2.show_state
+   # sleep 1
 
-    puts "Laisson #{player1.name} lancer les hostilités..."
-    sleep 1
+   # puts "-------ROUND #{x}-------"
+  #  x += 1
 
-    puts player1.attacks(player2)
-        if player2.life_points<=0 
-        break
-        end
-    sleep 1
-    puts player2.attacks(player1)
-    sleep 1
-end
+ #   puts "Laisson #{player1.name} lancer les hostilités..."
+   # sleep 1
+
+  #  puts player1.attacks(player2)
+      #  if player2.life_points<=0 
+      #  break
+      #  end
+   # sleep 1
+  #  puts player2.attacks(player1)
+ #   sleep 1
+#end     
+
+
+
+
--- lib/player.rb
+++ lib/player.rb
@@ -34,6 +34,58 @@ class Player
     
 end
 
+class HumanPlayer < Player 
+    attr_accessor :weapon_level, :name
 
+    def initialize(name)
+        @weapon_level = 1
+        super(name)
+        @life_points = 100
+    end
+
+    def show_state
+        puts "Le joueur #{@name} a #{@life_points} points de vie et une arme de niveau #{@weapon_level}!"
+    end
+
+    def compute_damage
+        rand(1..6) * @weapon_level    
+    end
+
+    def search_weapon
+        new_level = rand(1..6) 
+        puts "Tu as trouvé une arme de niveau #{new_level}"
+        if new_level > @weapon_level
+            puts "Youhou ! elle est meilleure que ton arme actuelle : tu la prends."
+        else 
+            puts "M@*#$... elle n'est pas mieux que ton arme actuelle..."
+        end
+
+    end
+
+    def search_health_pack
+        search = rand(1..6)
+        puts search
+
+        if search == 1
+            puts "Tu n'as rien trouvé... "
+        elsif search == 2 || 3 || 4 || 5
+            puts "Bravo, tu as trouvé un pack de +50 points de vie !"
+            if @life_points + 50 > 100
+                @life_points = 100
+            else
+                @life_points += 50
+            end
+        elsif search == 6
+            puts "Waow, tu as trouvé un pack de +80 points de vie !"
+            if @life_points + 80 > 100
+                @life_points = 100
+            else
+                @life_points += 80
+            end
+        end
+
+    end
+
+end
 
 
