e46aa898d62377b73d422c6335ad7a11fafdf4d9
MaevaGilles <maevagilles-web@hotmail.fr>
2022-10-18 17:38:09 +0200

    4eme commit

--- app.rb
+++ app.rb
@@ -9,28 +9,39 @@ player2 = Player.new("José")
 
 puts "[DEBUT DU JEU]"
 sleep 1
+
 puts "Ladies & Gentlemen BIENVENUUUUUE"
 sleep 1
+
 puts "À ma droite #{player1.name}!"
 sleep 1
+
 puts "À ma gauche #{player2.name}!"
 sleep 1
-while player1.life_points>0 && player2.life_points>0
-puts "Voici l'état de chaque joueur : "
-sleep 1
-puts player1.show_state
-sleep 1
-puts player2.show_state
-sleep 1
-puts "-------ROUND #{a + 1}) "
-puts "Laisson #{player1.name} lancer les hostilités..."
-sleep 1
-puts player1.attacks(player2)
- if player2.life_points<=0 
-    break
-    end
-sleep 1
-puts player2.attacks(player1)
-sleep 1
 
+x = 1
+while player1.life_points>0 && player2.life_points>0 
+
+    puts "Voici l'état de chaque joueur : "
+    sleep 1
+
+    puts player1.show_state
+    sleep 1
+
+    puts player2.show_state
+    sleep 1
+
+    puts "-------ROUND #{x}-------"
+    x += 1
+
+    puts "Laisson #{player1.name} lancer les hostilités..."
+    sleep 1
+
+    puts player1.attacks(player2)
+        if player2.life_points<=0 
+        break
+        end
+    sleep 1
+    puts player2.attacks(player1)
+    sleep 1
 end
